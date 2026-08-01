1c06fc9183d9dc19c289555326af12609aad6ed4
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-01 18:41:11 +0330

    Add confirmed customer demo links

--- links.js
+++ links.js
@@ -11,6 +11,18 @@ const confirmedProjectLinks = {
   306: 'https://team.smarbiz.sbs/',
   307: 'https://pay.smarbiz.sbs/',
   309: 'https://aplus-solution.de/wiw',
+  403: 'https://kayi-haustechnik.pages.dev/',
+  404: 'https://rot-weiss-frankfurt.pages.dev/',
+  406: 'https://manuel-hoefel.pages.dev/',
+  407: 'https://aymantraining.pages.dev/',
+  408: 'https://shervinaesthetics.pages.dev/',
+  409: 'https://citaimmobilien.pages.dev/',
+  410: 'https://drwickler.pages.dev/',
+  411: 'https://pro-clean-aschaffenburg.pages.dev/',
+  412: 'https://niederrad.pages.dev/',
+  413: 'https://hausaloys.pages.dev/',
+  414: 'https://seniorenheim.pages.dev/',
+  415: 'https://puracai.pages.dev/',
   601: 'http://cards.smarbiz.sbs/'
 };
 
