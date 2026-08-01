e3e321ced763e876bc85acf21e7f3a6c3c8a7656
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-01 18:41:19 +0330

    Add customer demo URLs

---
+++ migrations/0003_customer_demo_links.sql
@@ -0,0 +1,14 @@
+PRAGMA foreign_keys = ON;
+
+UPDATE projects SET url = 'https://kayi-haustechnik.pages.dev/' WHERE id = 403;
+UPDATE projects SET url = 'https://rot-weiss-frankfurt.pages.dev/' WHERE id = 404;
+UPDATE projects SET url = 'https://manuel-hoefel.pages.dev/' WHERE id = 406;
+UPDATE projects SET url = 'https://aymantraining.pages.dev/' WHERE id = 407;
+UPDATE projects SET url = 'https://shervinaesthetics.pages.dev/' WHERE id = 408;
+UPDATE projects SET url = 'https://citaimmobilien.pages.dev/' WHERE id = 409;
+UPDATE projects SET url = 'https://drwickler.pages.dev/' WHERE id = 410;
+UPDATE projects SET url = 'https://pro-clean-aschaffenburg.pages.dev/' WHERE id = 411;
+UPDATE projects SET url = 'https://niederrad.pages.dev/' WHERE id = 412;
+UPDATE projects SET url = 'https://hausaloys.pages.dev/' WHERE id = 413;
+UPDATE projects SET url = 'https://seniorenheim.pages.dev/' WHERE id = 414;
+UPDATE projects SET url = 'https://puracai.pages.dev/' WHERE id = 415;
